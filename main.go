f4276d080c8e9a9d3324ab65bc0b9ae2c16308ce
Miss-you <snowfly1993@gmail.com>
2021-02-03 16:16:11 +0800

    1. 支持读取文件，-f命令 2. 支持指定url，-url命令

--- main.go
+++ main.go
@@ -4,24 +4,46 @@ import (
 	"fmt"
 	"flag"
 	"strings"
+	"os"
+	"io/ioutil"
 
 	"github.com/gorilla/websocket"
 )
 
+func ReadAll(filePth string) ([]byte, error) {
+	f, err := os.Open(filePth)
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(f)
+}
+
 //func Dial(url_, protocol, origin string) (ws *Conn, err error)
 func main() {
-	url := "ws://service-j3301z72-1255960573.shjr.apigw.tencentcs.com:80/ws"
+	url := flag.String("url", "ws://service-j3301z72-1255960573.shjr.apigw.tencentcs.com:80/ws", "url")
 	msgLen := flag.Int("n", 10, "msg len")
+	filePath := flag.String("f", "", "file path")
 	flag.Parse()
 
-	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
+	ws, _, err := websocket.DefaultDialer.Dial(*url, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer ws.Close()
 
-	msg := strings.Repeat("a", *msgLen)
+	msg := ""
+	if *filePath == "" {
+		msg = strings.Repeat("a", *msgLen)
+	} else {
+		msgBuff, err := ReadAll(*filePath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		msg = string(msgBuff)
+	}
 
 	err = ws.WriteMessage(websocket.TextMessage, []byte(msg))
 	if err != nil {
